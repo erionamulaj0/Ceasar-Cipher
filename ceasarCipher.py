162c47c9d1613c951ed8ead0f0de1782f39da157
Eriona Mulaj <erionamulaj1@gmail.com>
2024-07-24 21:46:29 +0200

    Inital commit

---
+++ ceasarCipher.py
@@ -0,0 +1,20 @@
+def encrypt(text, s):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)
+
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+
+    return result
+
+
+text = "this is a random text"
+s = 9
+print("Text : " + text)
+print("Shift : " + str(s))
+print("Cipher: " + encrypt(text, s))
